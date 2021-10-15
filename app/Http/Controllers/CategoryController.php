6659c5623394f402e284711fd9a0042e55ddc93c
mohammad yaseen <mohammadyaseen223@gmail.com>
2021-10-15 16:23:07 +0500

    minor fix

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,7 +11,7 @@ class CategoryController extends Controller
     public function addCategory(Request $req) {
         
         $category = new Category;
-        $category->category_name = $req->category_name;
+        $category->name = $req->category_name;
         $category->save();
     
         return redirect()->back()->with('success','Category Added Succesfully!');
@@ -21,7 +21,7 @@ class CategoryController extends Controller
         
         $subCategory = new SubCategory;
         $subCategory->name = $req->name;
-        $subCategory->parent_category = $req->parent_category;
+        $subCategory->category_id = $req->parent_category;
         $subCategory->save();
     
         return redirect()->back()->with('success','Category Added Succesfully!');
--- app/SubCategory.php
+++ app/SubCategory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubCategory extends Model
 {
-    public function parent_category_id() {
-        return $this->belongsTo(Cateogry::class);
+    public function category() {
+        return $this->belongsTo(Category::class);
     }
 }
