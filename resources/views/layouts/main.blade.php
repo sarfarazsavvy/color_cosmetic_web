7aef98a6cc9af2d73d92dd16b0dc870c6cb58058
bilalariff <bilaljamoot1995@gmail.com>
2021-10-15 19:04:33 +0500

    UI implementation in progress

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,19 +1,20 @@
-<!doctype html>
-<html lang="en">
-  <head>
+<!DOCTYPE html>
+<html>
 
-    @include('layouts.head')
-    
-    <title>Hello, world!</title>
+<!-- HEAD SECTION -->
+@include('layouts.head')
+<!-- HEAD ENDS SECTION -->
+@include('layouts.nav')
 
-    @yield('styles')
-  </head>
-  <body>
+<body>
 
-    @yield('body')
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+        @yield('main')
+	</div>	<!--/.main-->
 
+    <!-- SCRIPTS -->
     @include('layouts.scripts')
-    
-    @yield('custom-scripts')
-  </body>
+    @yield('scripts')
+	<!-- SCRIPTS ENDS -->
+</body>
 </html>
