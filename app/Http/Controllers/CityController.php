1ca98ff443083333c66c0a8c09a5cebc4e426e78
bilalariff <bilaljamoot1995@gmail.com>
2021-10-20 13:47:28 +0500

    City CRUD in progresss

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    // Route::get('/all-cities', 'CityController@cities');
+// Route::get('/add-city-form', 'CityController@createCityForm');
+// Route::post('/add-city', 'CityController@createCity');
+
+    public function cities() {
+        $allcities = City::all();
+        return view('all-cities', compact('allCities'));
+    }
+
+    public function createCityForm() {
+        return view('add-cities');
+    }
+
+    public function createCity(Request $req) {
+        
+    }
+
+
+}
---
+++ resources/views/all-cities.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('main')
+
+<div class="container py-5 my-5">
+    <div class="row">
+        <div class="col-12">
+            <h1>All Cities</h1>
+        </div>
+    </div>
+    <div class="col-12">
+            <table class="table">
+                <thead class="table-dark">
+                    <th>ID</th>
+                    <th>Name</th>
+                </thead>
+                <tbody>
+                    <!-- @foreach( $categories as $category )
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->name}}</td>
+                    </tr>
+                    @endforeach -->
+                </tbody>
+            </table>
+        </div>
+</div>
+
+@endsection
