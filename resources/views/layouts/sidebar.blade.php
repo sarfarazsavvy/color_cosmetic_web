66e6cc74a43cbbea32b9cb3647f5b4de9bccecc1
bilalariff <bilaljamoot1995@gmail.com>
2021-11-02 10:59:31 +0500

    Work day 2 November, 2021

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,6 +7,7 @@
 
     <ul class="nav nav-pills flex-column mb-auto menu p-0">
 
+        <!-- Beauty Advisor -->
         <li class="nav-item">
             <a class="{{ request()->segment(1)=='all-ba-girls' ? 'active' : '' }}" href="" data-bs-toggle="collapse" data-bs-target="#sidebar-menu-1"
                 aria-expanded="false">
@@ -27,6 +28,7 @@
             </li>
         </ul>
 
+        <!-- Sales -->
         <li class="nav-item">
             <a href="" data-bs-toggle="collapse" data-bs-target="#sidebar-sales" aria-expanded="false">
                 <span>Sales</span>
@@ -38,8 +40,18 @@
             <li class="children-item {{ request()->segment(1)=='all-products' ? 'active' : '' }}">
                 <a class="" href="/all-products"><i class="fa fa-arrow-right"></i>BA Sales</a>
             </li>
+        </ul>
+
+        <li class="nav-item">
+            <a href="" data-bs-toggle="collapse" data-bs-target="#sidebar-sales" aria-expanded="false">
+                <span>Sales</span>
+                <i class="fa fa-plus"></i>
+            </a>
+        </li>
+        
+        <ul class="children collapse" id="sidebar-sales">
             <li class="children-item {{ request()->segment(1)=='all-products' ? 'active' : '' }}">
-                <a class="" href="{{route('pending_sales')}}"><i class="fa fa-arrow-right"></i>Pending Sales</a>
+                <a class="" href="/all-products"><i class="fa fa-arrow-right"></i>BA Sales</a>
             </li>
         </ul>
 
@@ -123,104 +135,6 @@
                 <a href="/add-sub-category-form"><i class="fa fa-arrow-right"></i>Add Sub Category</a>
             </li>
         </ul>
-
-        <!-- <li class="nav-item {{ request()->segment(1)=='home' ? 'active' : '' }}"><a href="/home"><em
-                    class="fa fa-dashboard">&nbsp;</em>Dashboard</a></li>
-        <li class="parent nav-item">
-            <a class="collapsed" data-bs-toggle="collapse" data-bs-target="#beauty-advisors" aria-expanded="false">
-                <em class="fa fa-navicon">&nbsp;</em> Beauty Advisors <span data-toggle="collapse" href="#sub-item-1"
-                    class="icon pull-right"><em class="fa fa-plus"></em></span>
-            </a>
-
-            <ul class="children collapse" id="beauty-advisors">
-                <li class=""><a class="{{ request()->segment(1)=='all-ba-girls' ? 'active' : '' }}" href="/all-ba-girls"
-                        id="all-ba-girls">
-                        <span class="fa fa-arrow-right">&nbsp;</span>All Beauty Advisors</a>
-                </li>
-                <li><a class="{{ request()->segment(1)=='beauty-advisors' ? 'active' : '' }}" href="/beauty-advisors">
-                        <span class="fa fa-arrow-right">&nbsp;</span>Add Beauty Advisor</a>
-                </li>
-                <li><a class="{{ request()->segment(1)=='assign-store-to-beauty-advisor-form' ? 'active' : '' }}"
-                        href="/assign-store-to-beauty-advisor-form">
-                        <span class="fa fa-arrow-right">&nbsp;</span>Assign Store to Beauty
-                        Advisor</a>
-                </li>
-            </ul>
-        </li>
-        <li class="parent nav-item"><a class="collapsed" data-bs-toggle="collapse" data-bs-target="#products"
-                aria-expanded="false">
-                <em class="fa fa-navicon">&nbsp;</em> Products <span data-toggle="collapse" href="#sub-item-5"
-                    class="icon pull-right"><em class="fa fa-plus"></em></span>
-            </a>
-            <ul class="children collapse" id="products">
-                <li><a class="{{ request()->segment(1)=='all-products' ? 'active' : '' }}" href="/all-products">
-                        <span class="fa fa-arrow-right">&nbsp;</span>All Products</a>
-                </li>
-                <li><a class="{{ request()->segment(1)=='add-products-form' ? ' active' : '' }}"
-                        href="/add-products-form">
-                        <span class="fa fa-arrow-right">&nbsp;</span>Add Products</a>
-                </li>
-            </ul>
-        </li>
-        <li class="parent nav-item"><a class="collapsed" data-bs-toggle="collapse" data-bs-target="#stores"
-                aria-expanded="false">
-                <em class="fa fa-navicon">&nbsp;</em> Stores <span data-toggle="collapse" href="#sub-item-2"
-                    class="icon pull-right"><em class="fa fa-plus"></em></span>
-            </a>
-            <ul class="children collapse" id="stores">
-                <li><a class="{{ request()->segment(1)=='all-stores' ? 'active' : '' }}" href="/all-stores">
-                        <span class="fa fa-arrow-right">&nbsp;</span> All Stores</a>
-                </li>
-                <li><a class="{{ request()->segment(1)=='add-store-form' ? 'active' : '' }}" href="/add-store-form">
-                        <span class="fa fa-arrow-right">&nbsp;</span> Create Stores</a>
-                </li>
-            </ul>
-        </li>
-        <li class="parent nav-item"><a class="collapsed" data-bs-toggle="collapse" data-bs-target="#city"
-                aria-expanded="false">
-                <em class="fa fa-navicon">&nbsp;</em> City <span data-toggle="collapse" href="#sub-item-3"
-                    class="icon pull-right"><em class="fa fa-plus"></em></span>
-            </a>
-            <ul class="children collapse" id="city">
-                <li><a class="{{ request()->segment(1)=='all-cities' ? 'active' : '' }}" href="/all-cities">
-                        <span class="fa fa-arrow-right">&nbsp;</span>All Cities</a>
-                </li>
-                <li><a class="{{ request()->segment(1)=='add-city-form' ? 'active' : '' }}" href="/add-city-form">
-                        <span class="fa fa-arrow-right">&nbsp;</span>Add Cities</a>
-                </li>
-            </ul>
-        </li>
-        <li class="parent nav-item"><a class="collapsed" data-bs-toggle="collapse" data-bs-target="#categories"
-                aria-expanded="false">
-                <em class="fa fa-navicon">&nbsp;</em> Categories <span data-toggle="collapse" href="#sub-item-4"
-                    class="icon pull-right"><em class="fa fa-plus"></em></span>
-            </a>
-            <ul class="children collapse" id="categories">
-                <li><a class="{{ request()->segment(1)=='all-categories' ? 'active' : '' }}" href="/all-categories">
-                        <span class="fa fa-arrow-right">&nbsp;</span>All Categories</a>
-                </li>
-                <li><a class="{{ request()->segment(1)=='add-category-form' ? 'active' : '' }}"
-                        href="/add-category-form">
-                        <span class="fa fa-arrow-right">&nbsp;</span>Add Categories</a>
-                </li>
-            </ul>
-        </li>
-        <li class="parent nav-item"><a class="collapsed" data-bs-toggle="collapse" data-bs-target="#sub-categories"
-                aria-expanded="false">
-                <em class="fa fa-navicon">&nbsp;</em>Sub Categories <span data-toggle="collapse" href="#sub-item-6"
-                    class="icon pull-right"><em class="fa fa-plus"></em></span>
-            </a>
-            <ul class="children collapse" id="sub-categories">
-                <li><a class="{{ request()->segment(1)=='all-sub-categories' ? 'active' : '' }}"
-                        href="/all-sub-categories">
-                        <span class="fa fa-arrow-right">&nbsp;</span>All Sub Categories</a>
-                </li>
-                <li><a class="{{ request()->segment(1)=='add-sub-category-form' ? 'active' : '' }}"
-                        href="/add-sub-category-form">
-                        <span class="fa fa-arrow-right">&nbsp;</span>Add Sub Categories</a>
-                </li>
-            </ul>
-        </li> -->   
         
         <form action="{{route('logout')}}" method="post"> @csrf
             <li><button class="btn sidebar-logout-btn" type="submit" href="" onClick=""><em
