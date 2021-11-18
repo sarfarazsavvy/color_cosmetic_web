6ce0894def7b922d2cde7f488d0502088af7b810
bilalariff <bilaljamoot1995@gmail.com>
2021-11-18 17:26:28 +0500

    code deleted

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,34 +28,4 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function regenerate_products_lists(){
-        
-        //first delete all old stock, reset stock;
-        $storestock = StoreStock::all();
-        foreach($storestock as $s)
-        {
-            $s->delete();
-        }
-
-        //fetch all existing stores
-        $stores = Store::all();
-        //fetch all products
-        $products = Product::all();
-        //iterate through each store so that we can add peroducts in that store
-        foreach($stores as $store)
-        {   // iterate through each product to add it
-            foreach($products as $product)
-            {   // adding specific product stock to specific store with zero qty
-                $storeStock = new StoreStock;
-                $storeStock->store_id = $store->id;
-                $storeStock->product_id = $product->id;
-                $storeStock->price = $product->price; //from proddutc
-                $storeStock->quantity = 0;
-                $storeStock->save();
-
-            }
-        }
-            
-
-    }
 }
